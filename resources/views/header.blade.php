73564c4f0b2d1fc360b5c19f780e63174896e994
ShinoharaTabito <64075243+ShinoharaTabito@users.noreply.github.com>
2020-07-22 23:55:56 +0900

    creste_header.blade.php

---
+++ resources/views/header.blade.php
@@ -0,0 +1,8 @@
+@section('header')
+    <div class="top-header">就活生のためのポートフォリオサイト
+      <a href="{{url('/new')}}">新規登録</a>
+      <a href="{{url('#')}}">ログイン</a>
+    </div>
+    <div class="header-leftbar">blogのタグ一覧バー(左に現れる)</div>
+    <div class="header-rightbar">ポートフォリオのタグ一覧バー(右に現われる)</div>
+@endsection
--- resources/views/portfolio/index.blade.php
+++ resources/views/portfolio/index.blade.php
@@ -2,15 +2,6 @@
 
 @section('title', "トップページ")
 
-@section('header')
-    <div class="top-header">就活生のためのポートフォリオサイト
-      <a href="{{url('/new')}}">新規登録</a>
-      <a href="{{url('#')}}">ログイン</a>
-    </div>
-    <div class="header-leftbar">blogのタグ一覧バー(左に現れる)</div>
-    <div class="header-rightbar">ポートフォリオのタグ一覧バー(右に現われる)</div>
-@endsection
-
 @section('content')
     <div class="top-content">
       <p class="title">Laravel Blogのトップページです。</p>
